3a7f313d4a12eeb6f71561be91155028d1edd10d
hegotit <153287866+hegotit@users.noreply.github.com>
2024-07-05 17:27:26 +0800

    Add prism

--- src/rime.rs
+++ src/rime.rs
@@ -2,4 +2,5 @@ pub mod algo;
 pub(crate) mod candidate;
 pub(crate) mod common;
 pub mod config;
+pub(crate) mod dict;
 pub(crate) mod language;
---
+++ src/rime/dict/mapped_file.rs
@@ -0,0 +1,73 @@
+use memmap2::{MmapMut, MmapOptions};
+use std::fs::File;
+use std::io::{Error, ErrorKind, Result};
+
+use crate::rime::common::PathExt;
+
+pub(crate) struct MappedFile {
+    file: File,
+    mmap: Option<MmapMut>,
+    size: usize,
+}
+
+impl MappedFile {
+    pub(crate) fn new(file_path: &PathExt) -> Result<Self> {
+        let file = File::options()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(file_path)?;
+
+        let mmap = unsafe { MmapOptions::new().map_mut(&file)? };
+        Ok(Self {
+            file,
+            mmap: Some(mmap),
+            size: 0,
+        })
+    }
+
+    fn size(&self) -> usize {
+        self.size
+    }
+
+    fn read(&self, offset: usize, length: usize) -> Option<&[u8]> {
+        match &self.mmap {
+            Some(mmap) if offset + length <= self.size => Some(&mmap[offset..offset + length]),
+            _ => None,
+        }
+    }
+
+    fn write(&mut self, offset: usize, data: &[u8]) -> Result<()> {
+        let size = data.len();
+        match &mut self.mmap {
+            Some(mmap) /*if offset + size <= self.size*/ => {
+                mmap/*[offset..offset + data.len()]*/.copy_from_slice(data);
+                mmap.flush()?;
+                Ok(())
+            }
+            // Some(_) => Err(Error::new(ErrorKind::InvalidInput, "Write out of bounds")),
+            None => Err(Error::new(
+                ErrorKind::Other,
+                "Memory map is not initialized",
+            )),
+        }
+    }
+}
+
+#[test]
+fn test() -> Result<()> {
+    let file_path = PathExt::new("example.txt");
+    // 创建一个映射文件
+    let mut mapped_file = MappedFile::new(&file_path)?;
+
+    // 向文件中写入数据
+    let data = b"Hello, Rust!";
+    mapped_file.write(0, data)?;
+
+    // 读取文件中的数据
+    if let Some(contents) = mapped_file.read(0, data.len()) {
+        println!("Read from file: {:?}", std::str::from_utf8(contents));
+    }
+
+    Ok(())
+}
