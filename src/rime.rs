7cc6dc4fbfe57611538b558a4428bfdc96ddb358
hegotit <153287866+hegotit@users.noreply.github.com>
2024-07-06 21:09:19 +0800

    Add key_event

--- src/rime.rs
+++ src/rime.rs
@@ -1,7 +1,21 @@
 pub mod algo;
 pub(crate) mod candidate;
+pub(crate) mod commit_history;
 pub(crate) mod common;
+pub(crate) mod component;
+pub(crate) mod composition;
 pub mod config;
+pub(crate) mod context;
 pub(crate) mod dict;
+pub(crate) mod filter;
+pub mod key_event;
 pub mod key_table;
 pub(crate) mod language;
+pub(crate) mod menu;
+pub(crate) mod registry;
+pub(crate) mod resource;
+pub(crate) mod segmentation;
+pub(crate) mod service;
+pub(crate) mod translation;
+
+pub const RIME_VERSION: &str = "1.11.2";
