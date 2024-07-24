0e2fc36bcc7f08b39c46e66ad6e7816b9721b931
hegotit <153287866+hegotit@users.noreply.github.com>
2024-07-24 15:11:05 +0800

    Add composition

--- src/rime.rs
+++ src/rime.rs
@@ -2,6 +2,7 @@ pub(crate) mod algo;
 pub(crate) mod candidate;
 pub(crate) mod common;
 pub(crate) mod component;
+pub(crate) mod composition;
 pub(crate) mod config;
 pub(crate) mod dict;
 pub(crate) mod filter;
