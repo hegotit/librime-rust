608a2bac0c16021aea0240a68c62bd95c7f61d3c
hegotit <153287866+hegotit@users.noreply.github.com>
2024-11-22 17:31:16 +0800

    temp3

--- src/rime/dict/string_table.rs
+++ src/rime/dict/string_table.rs
@@ -15,125 +15,125 @@ pub struct StringTable {
 }
 
 impl StringTable {
-    pub fn new() -> Self {
-        Self {
-            trie: TrieBuilder::new().build(),
-            id_to_string: HashMap::new(),
-        }
-    }
+    //pub fn new() -> Self {
+    //    Self {
+    //        trie: TrieBuilder::new().build(),
+    //        id_to_string: HashMap::new(),
+    //    }
+    //}
 
-    pub fn has_key(&self, key: &str) -> bool {
-        self.trie.exact_match(key.as_bytes())
-    }
+    //pub fn has_key(&self, key: &str) -> bool {
+    //    self.trie.exact_match(key.as_bytes())
+    //}
 
-    pub fn lookup(&self, key: &str) -> StringId {
-        //let mut agent = Agent::new();
-        //agent.set_query(key);
-        //if self.trie.lookup(&mut agent) {
-        //    agent.key().id() as StringId
-        //} else {
-        //    INVALID_STRING_ID
-        //}
+    //pub fn lookup(&self, key: &str) -> StringId {
+    //    //let mut agent = Agent::new();
+    //    //agent.set_query(key);
+    //    //if self.trie.lookup(&mut agent) {
+    //    //    agent.key().id() as StringId
+    //    //} else {
+    //    //    INVALID_STRING_ID
+    //    //}
 
-        if let Some(_) = self.trie.exact_match(key.as_bytes()) {
-            Some(self.trie.exact_match(key.as_bytes()).unwrap() as StringId)
-        } else {
-            None
-        }
-    }
+    //    if let Some(_) = self.trie.exact_match(key.as_bytes()) {
+    //        Some(self.trie.exact_match(key.as_bytes()).unwrap() as StringId)
+    //    } else {
+    //        None
+    //    }
+    //}
 
-    pub fn common_prefix_match(&self, query: &str) -> Vec<StringId> {
-        let mut agent = Agent::new();
-        agent.set_query(query);
-        let mut result = Vec::new();
-        while self.trie.common_prefix_search(&mut agent) {
-            result.push(agent.key().id() as StringId);
-        }
-        result
-    }
+    //pub fn common_prefix_match(&self, query: &str) -> Vec<StringId> {
+    //    let mut agent = Agent::new();
+    //    agent.set_query(query);
+    //    let mut result = Vec::new();
+    //    while self.trie.common_prefix_search(&mut agent) {
+    //        result.push(agent.key().id() as StringId);
+    //    }
+    //    result
+    //}
 
-    pub fn predict(&self, query: &str) -> Vec<StringId> {
-        let mut agent = Agent::new();
-        agent.set_query(query);
-        let mut result = Vec::new();
-        while self.trie.predictive_search(&mut agent) {
-            result.push(agent.key().id() as StringId);
-        }
-        result
-    }
+    //pub fn predict(&self, query: &str) -> Vec<StringId> {
+    //    let mut agent = Agent::new();
+    //    agent.set_query(query);
+    //    let mut result = Vec::new();
+    //    while self.trie.predictive_search(&mut agent) {
+    //        result.push(agent.key().id() as StringId);
+    //    }
+    //    result
+    //}
 
-    pub fn get_string(&self, string_id: StringId) -> Option<String> {
-        let mut agent = Agent::new();
-        agent.set_query(string_id as usize);
-        if self.trie.reverse_lookup(&mut agent).is_ok() {
-            Some(agent.key().to_str().unwrap().to_string())
-        } else {
-            None
-        }
-    }
+    //pub fn get_string(&self, string_id: StringId) -> Option<String> {
+    //    let mut agent = Agent::new();
+    //    agent.set_query(string_id as usize);
+    //    if self.trie.reverse_lookup(&mut agent).is_ok() {
+    //        Some(agent.key().to_str().unwrap().to_string())
+    //    } else {
+    //        None
+    //    }
+    //}
 
-    pub fn num_keys(&self) -> usize {
-        self.trie.num_keys()
-    }
+    //pub fn num_keys(&self) -> usize {
+    //    self.trie.num_keys()
+    //}
 
-    pub fn binary_size(&self) -> usize {
-        self.trie.io_size()
-    }
+    //pub fn binary_size(&self) -> usize {
+    //    self.trie.io_size()
+    //}
 }
 
-pub struct StringTableBuilder {
-    keys: Keyset,
-    references: VecDeque<Option<StringId>>,
-}
+//pub struct StringTableBuilder {
+//    keys: Keyset,
+//    references: VecDeque<Option<StringId>>,
+//}
 
-impl StringTableBuilder {
-    pub fn new() -> Self {
-        Self {
-            keys: Keyset::new(),
-            references: VecDeque::new(),
-        }
-    }
+//impl StringTableBuilder {
+//    pub fn new() -> Self {
+//        Self {
+//            keys: Keyset::new(),
+//            references: VecDeque::new(),
+//        }
+//    }
 
-    pub fn add(&mut self, key: &str, weight: f64, reference: Option<&mut StringId>) {
-        self.keys.push(key, weight as f32);
-        if let Some(ref mut ref_value) = reference {
-            self.references.push_back(Some(*ref_value));
-        } else {
-            self.references.push_back(None);
-        }
-    }
+//    pub fn add(&mut self, key: &str, weight: f64, reference: Option<&mut StringId>) {
+//        self.keys.push(key, weight as f32);
+//        if let Some(ref mut ref_value) = reference {
+//            self.references.push_back(Some(*ref_value));
+//        } else {
+//            self.references.push_back(None);
+//        }
+//    }
 
-    pub fn clear(&mut self) {
-        self.keys.clear();
-        self.references.clear();
-    }
+//    pub fn clear(&mut self) {
+//        self.keys.clear();
+//        self.references.clear();
+//    }
 
-    pub fn build(&mut self) -> Trie {
-        let trie = Trie::build(&self.keys);
-        self.update_references(&trie);
-        trie
-    }
+//    pub fn build(&mut self) -> Trie {
+//        let trie = Trie::build(&self.keys);
+//        self.update_references(&trie);
+//        trie
+//    }
 
-    fn update_references(&mut self, trie: &Trie) {
-        let mut agent = Agent::new();
-        for i in 0..self.keys.len() {
-            if let Some(ref_value) = self.references.pop_front() {
-                if let Some(mut ref_value) = ref_value {
-                    agent.set_query(self.keys[i].as_str());
-                    if trie.lookup(&mut agent) {
-                        ref_value = agent.key().id() as StringId;
-                    }
-                }
-            }
-        }
-    }
+//    fn update_references(&mut self, trie: &Trie) {
+//        let mut agent = Agent::new();
+//        for i in 0..self.keys.len() {
+//            if let Some(ref_value) = self.references.pop_front() {
+//                if let Some(mut ref_value) = ref_value {
+//                    agent.set_query(self.keys[i].as_str());
+//                    if trie.lookup(&mut agent) {
+//                        ref_value = agent.key().id() as StringId;
+//                    }
+//                }
+//            }
+//        }
+//    }
 
-    pub fn dump(&self, data: &mut [u8]) {
-        let size_needed = self.keys.io_size();
-        if data.len() < size_needed {
-            eprintln!("Insufficient memory to dump string table.");
-            return;
-        }
-        self.keys.write(&mut data[..size_needed]);
-    }
-}
+//    pub fn dump(&self, data: &mut [u8]) {
+//        let size_needed = self.keys.io_size();
+//        if data.len() < size_needed {
+//            eprintln!("Insufficient memory to dump string table.");
+//            return;
+//        }
+//        self.keys.write(&mut data[..size_needed]);
+//    }
+//}
--- src/rime/dict/table.rs
+++ src/rime/dict/table.rs
@@ -1,15 +1,13 @@
-////use crate::rime::common::PathExt;
+use log::info;
 
-//use log::info;
+use crate::rime::algo::SyllableId;
+use crate::rime::common::PathExt;
+use crate::rime::dict::vocabulary::{Syllabary as SyllSet, Vocabulary};
 
-//use crate::rime::algo::SyllableId;
-//use crate::rime::common::PathExt;
-//use crate::rime::dict::vocabulary::{Syllabary as SyllSet, Vocabulary};
-
-//#[derive(Clone, Debug)]
-//pub struct StringType {
-//    value: i32,
-//}
+#[derive(Clone, Debug)]
+pub struct StringType {
+    value: i32,
+}
 
 //impl StringType {
 //    pub fn str(&self) -> String {
@@ -21,7 +19,7 @@
 //    }
 //}
 
-//type Syllabary = Vec<StringType>;
+type Syllabary = Vec<StringType>;
 
 //pub type Code = Vec<SyllableId>;
 
@@ -65,121 +63,120 @@
 
 //pub type Index = HeadIndex;
 
-//struct Metadata {
-//    format: String,
-//    dict_file_checksum: u32,
-//    num_syllables: u32,
-//    num_entries: u32,
-//    string_table_size: u32,
-//}
+struct Metadata {
+    format: String,
+    dict_file_checksum: u32,
+    num_syllables: u32,
+    num_entries: u32,
+    string_table_size: u32,
+}
 
-//pub struct Table {
-//    file_path: PathExt,
-//    metadata: Option<Metadata>,
-//    syllabary: Option<Syllabary>,
-//    string_table_builder : Option<>
-//}
+pub struct Table {
+    file_path: PathExt,
+    metadata: Option<Metadata>,
+    syllabary: Option<Syllabary>,
+    //string_table_builder: Option,
+}
 
 ////type Vocabulary = HashMap<usize, Vec<ShortDictEntry>>;
 
-//impl Table {
-//    pub fn new(file_path: PathExt) -> Self {
-//        Self {
-//            file_path,
-//            metadata: None,
-//            syllabary: None,
-//        }
-//    }
+impl Table {
+    pub fn new(file_path: PathExt) -> Self {
+        Self {
+            file_path,
+            metadata: None,
+            syllabary: None,
+        }
+    }
 
-//    //    fn remove(&mut self) {
-//    //        // 清空内容
-//    //        self.syllabary.clear();
-//    //        self.vocabulary.clear();
-//    //    }
+    //    //    fn remove(&mut self) {
+    //    //        // 清空内容
+    //    //        self.syllabary.clear();
+    //    //        self.vocabulary.clear();
+    //    //    }
 
-//    pub fn build(
-//        &mut self,
-//        syllabary: &SyllSet,
-//        vocabulary: &Vocabulary,
-//        num_entries: usize,
-//        dict_file_checksum: u32,
-//    ) -> bool {
-//        let num_syllables = syllabary.len();
-//        //let estimated_file_size = K_RESERVED_SIZE + 32 * num_syllables + 64 * num_entries;
+    pub fn build(
+        &mut self,
+        syllabary: &SyllSet,
+        vocabulary: &Vocabulary,
+        num_entries: usize,
+        dict_file_checksum: u32,
+    ) -> bool {
+        let num_syllables = syllabary.len();
 
-//        info!("building table.");
-//        info!("num syllables: {}", num_syllables);
-//        info!("num entries: {}", num_entries);
+        info!("building table.");
+        info!("num syllables: {}", num_syllables);
+        info!("num entries: {}", num_entries);
 
-//        info!("creating metadata.");
+        info!("creating metadata.");
 
-//        self.metadata = Some(Metadata {
-//            format: String::new(),
-//            dict_file_checksum,
-//            num_syllables: num_syllables as u32,
-//            num_entries: num_entries as u32,
-//            string_table_size: 0,
-//        });
+        self.metadata = Some(Metadata {
+            format: String::new(),
+            dict_file_checksum,
+            num_syllables: num_syllables as u32,
+            num_entries: num_entries as u32,
+            string_table_size: 0,
+        });
 
-//        self.string_table_builder_ = StringTableBuilder::default();
+        info!("creating syllabary.");
 
-//        //println!("creating syllabary.");
-//        //self.syllabary = Some(syllabary.clone());
-//        //if let Some(metadata) = &mut self.metadata {
-//        //    metadata.syllabary = Some(syllabary.clone());
-//        //}
+        for syllable in syllabary {}
+        //self.syllabary = Some(syllabary.clone());
+        //if let Some(metadata) = &mut self.metadata {
+        //    metadata.syllabary = Some(syllabary.clone());
+        //}
 
-//        //println!("creating table index.");
-//        //self.index = Some(self.build_index(vocabulary, num_syllables));
-//        //if let Some(metadata) = &mut self.metadata {
-//        //    metadata.index = self.index.clone();
-//        //}
+        //println!("creating table index.");
+        //self.index = Some(self.build_index(vocabulary, num_syllables));
+        //if let Some(metadata) = &mut self.metadata {
+        //    metadata.index = self.index.clone();
+        //}
 
-//        //if !self.on_build_finish() {
-//        //    return false;
-//        //}
+        //if !self.on_build_finish() {
+        //    return false;
+        //}
 
-//        //if let Some(metadata) = &mut self.metadata {
-//        //    metadata.format = K_TABLE_FORMAT_LATEST.to_string();
-//        //}
+        //if let Some(metadata) = &mut self.metadata {
+        //    metadata.format = K_TABLE_FORMAT_LATEST.to_string();
+        //}
 
-//        true // 假设构建成功
-//    }
+        todo!()
+    }
 
-//    //    fn save(&self) -> bool {
-//    //        // 假设保存成功
-//    //        true
-//    //    }
+    //    //    fn save(&self) -> bool {
+    //    //        // 假设保存成功
+    //    //        true
+    //    //    }
 
-//    //    fn load(&mut self) -> bool {
-//    //        // 假设加载成功
-//    //        true
-//    //    }
+    //    //    fn load(&mut self) -> bool {
+    //    //        // 假设加载成功
+    //    //        true
+    //    //    }
 
-//    //    fn close(&mut self) {
-//    //        // 关闭文件等操作
-//    //    }
+    //    //    fn close(&mut self) {
+    //    //        // 关闭文件等操作
+    //    //    }
 
-//    fn get_syllable_by_id(&self, syllable_id: SyllableId) -> Option<&str> {
-//        if let Some(syllabary) = &self.syllabary {
-//            if syllable_id < syllabary.len() as SyllableId {
-//                return Some(syllabary[syllable_id as usize].str_id());
-//            }
-//        }
-//        None
-//    }
+    //    fn get_syllable_by_id(&self, syllable_id: SyllableId) -> Option<&str> {
+    //        if let Some(syllabary) = &self.syllabary {
+    //            if syllable_id < syllabary.len() as SyllableId {
+    //                return Some(syllabary[syllable_id as usize].str_id());
+    //            }
+    //        }
+    //        None
+    //    }
 
-//    //    fn query_words(&self, code: usize) -> Option<&Vec<ShortDictEntry>> {
-//    //        self.vocabulary.get(&code)
-//    //    }
+    //    //    fn query_words(&self, code: usize) -> Option<&Vec<ShortDictEntry>> {
+    //    //        self.vocabulary.get(&code)
+    //    //    }
 
-//    //    fn query_phrases(&self, code: &[usize]) -> Option<Vec<&ShortDictEntry>> {
-//    //        let mut result = Vec::new();
-//    //        for c in code {
-//    //            if let Some(entries) = self.vocabulary.get(c) {
-//    //                result.extend(entries.iter());
-//    //            }
-//    //        }
-//    //        Some(result)
-//    //    }
-//}
+    //    //    fn query_phrases(&self, code: &[usize]) -> Option<Vec<&ShortDictEntry>> {
+    //    //        let mut result = Vec::new();
+    //    //        for c in code {
+    //    //            if let Some(entries) = self.vocabulary.get(c) {
+    //    //                result.extend(entries.iter());
+    //    //            }
+    //    //        }
+    //    //        Some(result)
+    //    //    }
+}
